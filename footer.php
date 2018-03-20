14e4a26cde156064d551cf7be8d0711796b503a8
Peter66666 <mistrhil@gmail.com>
2018-03-20 15:20:54 +0100

    fefef

---
+++ footer.php
@@ -0,0 +1,33 @@
+
+
+</div>
+<footer class="footer bg-dark text-white py-3">
+    <div class="container">
+
+
+        <div class="row">
+            <div class="col-md-3"><h5> Copyright </h5>
+                2018</div>
+            <div class="col-md-3"><h5> O nás </h5>
+                <ul>
+                    <a href="#"><li>první</li></a>
+                    <a href="#"><li>druhý</li></a>
+                    <a href="#"><li>třetí</li></a>
+                </ul></div>
+            <div class="col-md-3"><h5> Fakturační údaje </h5>
+                IČ: 865214<br/>
+                DIČ: 152864<br/>
+                Le Firma s.r.o<br/>
+                Komenského 123 15600
+            </div>
+            <div class="col-md-3"><h5> Kontakt </h5>
+                 Franta Frantovič<br/>
+                 <a href="mailto: franta@gmail.com"> franta@gmail.com </a><br/>
+                 <a href="tel: 666 555 444"> 666 555 444 </a>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title><?php the_title(); ?> | <?php echo bloginfo("name") ?> </title>
+        <link rel="STYLESHEET" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" media="screen" />
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <div class="container">
+        <h1>wazzup
+        </h1>
